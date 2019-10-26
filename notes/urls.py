ad47089b0d139c470f2022c5b7ac0312a95f0ee3
Amithnc <53530365+Amithnc@users.noreply.github.com>
2019-10-26 21:13:10 +0530

    update with template and modified dynanic loading of notes

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.homepage),
-    path('notes_cs-is/',views.notes),
+    path('notes/',views.notes),
 ]
--- notes/views.py
+++ notes/views.py
@@ -8,17 +8,16 @@ def homepage(request):
 def notes(request):
     context ={ }
     system=request.POST.get('system',None)
-    if system=="11":
-        notes=Note.objects.filter(branch_sem_id="11")
-        i=1
-        for note in notes:
-            if note.subject_name not in context.values():
-                context["subject"+str(i)]=note.subject_name
-                i=i+1
-        s=list(context.values())
-        context['subject']=s 
-        context['notes']=notes
+    notes=Note.objects.filter(branch_sem_id=system)
+    i=1
+    for note in notes:
+        if note.subject_name not in context.values():
+            context["subject"+str(i)]=note.subject_name
+            i=i+1
+    s=list(context.values())
+    context['subject']=s 
+    context['notes']=notes
                                
-        return render(request,'cs-is.html',context)
+    return render(request,'notes.html',context)
     
 
