257519137bce188c3f78f65d101eafe28fff15c4
Amithnc <53530365+Amithnc@users.noreply.github.com>
2019-12-01 14:37:21 +0530

    added choice for selecting branches

--- notes/models.py
+++ notes/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from django.conf import settings
 # Create your models here.
-def sample_view(request):
-    current_user = request.user
 class Note(models.Model):
-    module_no=models.CharField(max_length=1,default='',help_text=u"ENTER THE MODULE NUMBER")
+    module_no=models.CharField(max_length=3,default='',help_text=u"select the MODULE NUMBER")
     upload_file=models.FileField(help_text="UPLOAD CORRECT FILE         NOTE:U CANT CHANGE AFTER UPLOADING",upload_to='files/')
-    subject_name=models.CharField(help_text="ENTER THE SUBJECT NAME",max_length=10,default='')
-    added_by =models.CharField(help_text="ENTER YOUR NAME",max_length=10,default='')   
+    subject_name=models.CharField(help_text="ENTER THE SUBJECT NAME",max_length=30,default='')
+    added_by =models.CharField(help_text="ENTER YOUR NAME",max_length=10,default='')
     module_name=models.CharField(help_text="ENTER MODULE NAME",max_length=40,default='')
-    branch_sem_id=models.CharField(help_text="enter",max_length=2,default='')
+    branch_and_sem=models.CharField(help_text="choose branch and sem",max_length=100,default='',choices=[('cs/IS-sem3', 'cs/IS-sem3'),('cs/IS-sem4','cs/IS-sem4'),('cs/IS-sem5','cs/IS-sem5'),('cs/IS-sem6','cs/IS-sem6'),('ec-sem3', 'ec-sem3'),('ec-sem4','ec-sem4'),('ec-sem5','ec-sem5'),('ec-sem6','ec-sem6'),('civil-sem3', 'civil-sem3'),('civil-sem4','civil-sem4'),('civil-sem5','civil-sem5'),('civil-sem6','civil-sem6'),('mech-sem3', 'mech-sem3'),('mech-sem4','mech-sem4'),('mech-sem5','mech-sem5'),('mech-sem6','mech-sem6')])
 
     def __str__(self):
-         return self.subject_name+" Module  "+self.module_no +"    ADDED BY: "+self.added_by+self.branch_sem_id
+         return self.subject_name+" Module  "+self.module_no +"    ADDED BY: "+self.added_by+self.branch_and_sem
 
--- notes/views.py
+++ notes/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
 from .models import Note
-from array import *
-import string
 def homepage(request):
-    return render(request,'home.html')    
+    return render(request,'home.html')
 def notes(request):
     context ={ }
-    system=request.POST.get('system',None)
-    notes=Note.objects.filter(branch_sem_id=system)
+    system=request.POST.get('notes',None)
+    notes=Note.objects.filter(branch_and_sem=system)
     i=1
     for note in notes:
         if note.subject_name not in context.values():
             context["subject"+str(i)]=note.subject_name
             i=i+1
     s=list(context.values())
-    context['subject']=s 
+    context['subject']=s
     context['notes']=notes
-                               
+
     return render(request,'notes.html',context)
-    
+
 
