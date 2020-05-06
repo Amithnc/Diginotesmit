515ed68e589c646ba931f3952fc6fee0a290f148
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-05-06 21:39:35 +0530

    duplicate entry of subject is avoided

--- notes/models.py
+++ notes/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 # Create your models here.
 
-class ReadOnlyManager(models.Manager):
-    def get_queryset(self):
-        return super(ReadOnlyManager, self).get_queryset().annotate(combined=Concat('module_no','subject_name', output_field=CharField()))
-
 class Note(models.Model):
     module_no=models.CharField(max_length=3,default='',help_text=u"select the MODULE NUMBER")
     upload_file=models.FileField(help_text="UPLOAD CORRECT FILE         NOTE:U CANT CHANGE AFTER UPLOADING",upload_to='files/')
     subject_name=models.CharField(help_text="ENTER THE SUBJECT NAME",max_length=30,default='')
     added_by =models.CharField(help_text="ENTER YOUR NAME",max_length=10,default='')
     module_name=models.CharField(help_text="ENTER MODULE NAME",max_length=40,default='')
+    subject_code=models.CharField(help_text="ENTER subject code",max_length=20,default='')
     scheme=models.CharField(help_text="choose the scheme",max_length=10,default='',choices=[('2017','2017'),('2018','2018')])
     branch=models.CharField(help_text="choose branch",max_length=100,default='',choices=[('cs/IS', 'cs/IS'),('EC','EC'),('civil','civil'),('mech','mech'),('1styear','1styear')])
     sem=models.CharField(help_text="choose the sem",max_length=10,default='',choices=[('3','3'),('4','4'),('5','5'),('6','6')])
     SBSKEY=models.CharField("SBSKEY",max_length=30,blank=False,editable=False)
 
-    def save(self, *args, **kwargs): 
+    def save(self, *args, **kwargs):
         self.SBSKEY= self.scheme + self.branch + self.sem
+        self.subject_code=self.subject_code.lower()
+        self.subject_name=self.subject_name.lower()
         super(Note, self).save(*args, **kwargs)
     def __str__(self):
          return self.subject_name+" Module  "+self.module_no +"    ADDED BY: "+self.added_by+self.SBSKEY
--- notes/views.py
+++ notes/views.py
@@ -4,17 +4,22 @@ def homepage(request):
     return render(request,'home.html')
 def notes(request):
     context ={ }
+    codecontext ={ }
     system=request.POST.get('notes',None)
     notes=Note.objects.filter(SBSKEY=system)
     i=1
     for note in notes:
-        if note.subject_name not in context.values():
-            context["subject"+str(i)]=note.subject_name
-            i=i+1
+        if note.subject_code not in codecontext.values():
+            if note.subject_name not in context.values():
+                context["subject"+str(i)]=note.subject_name
+                codecontext["subject"+str(i)]=note.subject_code
+                i=i+1
     s=list(context.values())
-    context['subject']=s
+    subcode=list(codecontext.values())
+    context['code']=subcode
     context['notes']=notes
-
+    context['mainlist']=zip(s,subcode)
     return render(request,'notes.html',context)
 
-
+def handler404(request,exception):
+    return render(request, '404.html', locals())
