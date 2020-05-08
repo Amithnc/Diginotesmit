61a1c22fe6fb9b771b31176f3dffb5e9f63b2107
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-05-09 02:41:35 +0530

    added user specific view in admin dashboard

--- notes/admin.py
+++ notes/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import Note
-admin.site.register(Note)
 admin.site.index_title="add notes"
 admin.site.site_title="notes@mit"
 admin.site.site_header="notes administration"
 
+class NoteAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(added_by=request.user)
+admin.site.register(Note,NoteAdmin)
 
 
 
