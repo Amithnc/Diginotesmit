7528d6a0bb414b7d1228074c9bbcbbb9e5b87743
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-07-15 01:16:34 +0530

    show info if notes not avilable

--- notes/views.py
+++ notes/views.py
@@ -3,44 +3,50 @@ from .models import Note
 from gdstorage.storage import GoogleDriveStorage
 gd_storage = GoogleDriveStorage()
 def homepage(request):
-  #  a,b=gd_storage.listdir("google drive folder-(API)")       
-  #  print(a,b)#gives o/p a=list of directries and b=list of files 
     return render(request,'home.html')
 def notes(request):
     context ={ }
     codecontext ={ }
     system=request.POST.get('notes',None)
     notes=Note.objects.filter(SBSKEY=system).order_by('subject_code','module_no')
+    year=system[:4]
+    system=system[4:]
+    branch=['cs/IS','mech','civil','EC']
+    temp=system[-1:]
+    brnch=''
+    ch=''
+    if temp.isdigit():
+        for i in system:
+            ch=ch+i
+            if ch in branch:
+                brnch=(ch)
+    else:
+        brnch='1styear'
+        temp=system[-7:]
     i=1
-    sem=''
-    scheme=''
-    branch=''
     for note in notes:
         if note.subject_code not in codecontext.values():
             if note.subject_name not in context.values():
                 context["subject"+str(i)]=note.subject_name
                 codecontext["subject"+str(i)]=note.subject_code
-                sem=note.sem
-                scheme=note.scheme
-                branch=note.branch
-                i=i+1           
+                i=i+1
     s=list(context.values())
     subcode=list(codecontext.values())
     context['code']=subcode
     context['notes']=notes
     context['mainlist']=zip(s,subcode)
     context['subname']=s
-    context['sem']=sem
-    context['scheme']=scheme
-    context['branch']=branch
+    context['sem']=temp
+    context['scheme']=year
+    context['branch']=brnch
     return render(request,'notes.html',context)
-   
+
 def viewnotes(request):
     context={ }
     system=request.POST.get('viewnotes',None)
     path=gd_storage.url(system)
     context['url']=path
-    return render(request,'view.html',context) 
+    return render(request,'view.html',context)
 
 def handler404(request,exception):
     return render(request, '404.html', locals())
